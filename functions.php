a7d85e951b72474d25aca5b454f23b058e03f233
Serhii Kirilenko <ksmua85@gmail.com>
2019-02-11 00:57:40 +0200

    Try to create walker for list-group menu

--- functions.php
+++ functions.php
@@ -74,63 +74,108 @@ function rda_styles() {
       $output .= "\n$indent<ul class=\"panel myside-menu$submenu depth_$depth $collapse\">\n";
     }//start_lvl
   
-    // 
+    /*
+    // START ELEMENT OROGINAL WALKER
   
-public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-		if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
-			$t = '';
-			$n = '';
-		} else {
-			$t = "\t";
-			$n = "\n";
-		}
-		$indent = ( $depth ) ? str_repeat( $t, $depth ) : '';
+      public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+          if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+            $t = '';
+            $n = '';
+          } else {
+            $t = "\t";
+            $n = "\n";
+          }
+          $indent = ( $depth ) ? str_repeat( $t, $depth ) : '';
 
-		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
-    // Here is @list-group-item@ clas added
-    $classes[] = 'list-group-item menu-item-' . $item->ID;
+          $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+          // Here is @list-group-item@ clas added
+          $classes[] = 'list-group-item menu-item-' . $item->ID;
 
-    $args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
+          $args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
 
-    $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
-    $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : ''; 
-    
-    $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth );
-		$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+          $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+          $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : ''; 
+          
+          $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth );
+          $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
 
-		$output .= $indent . '<li' . $id . $class_names .'>';
+          $output .= $indent . '<li' . $id . $class_names .'>';
 
-		$atts = array();
-		$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
-		$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
-		$atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
-		$atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+          $atts = array();
+          $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+          $atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+          $atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+          $atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+        
+          $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+
+          $attributes = '';
+          foreach ( $atts as $attr => $value ) {
+            if ( ! empty( $value ) ) {
+              $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+              $attributes .= ' ' . $attr . '="' . $value . '"';
+            }
+          }
+
+          $title = apply_filters( 'the_title', $item->title, $item->ID );
+          
+          $title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
+
+          $item_output = $args->before;
+          $item_output .= '<a'. $attributes .'>';
+          $item_output .= $args->link_before . $title . $args->link_after;
+          $item_output .= '</a>';
+          $item_output .= $args->after;
+          
+          $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+        }
+        */
+        //START ELEMENT OROGINAL WALKER
+        
+        //START ELEMENT CUSTOM
+        function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0) {
+          $indent = ($depth) ? str_repeat("\t", $depth) : '';
+
+          $li_atributes = '';
+          $class_names = $value = '';
+
+          $classes = empty( $item->classes ) ? array() : (array)$item->classes;
+
+          $classes[] = ( $args->walker->has_children ) ? 'dropdown' : '';
+          $classes[] = ( $item->curent || $item->curent_item_anchestor) ? 'active' : '';
+          $classes[] = 'list-group-item menu-item-' . $item->ID; // Here @list-group-item@ clas added
+          if ( $depth && $args->walker->has_children ){
+            $classes[] = 'dropdown-submenu';
+          }
+
+          $class_names = join(  ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+          $class_names = 'class="' . esc_attr( $class_names ) . '"';
+
+          $id = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args);
+          $id = strlen( $id ) ? ' id="' . esc_attr($id) . '"' : '';
+
+          $output .= $indent . '<li' . $id . $value . $class_names . $li_atributes . '>';
+
+          $attributes = ! empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
+          $attributes = ! empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
+          $attributes = ! empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
+          $attributes = ! empty($item->url) ? ' href="' . esc_attr($item->url) . '"' : '';
+
+          $attributes .= ( $args->walker->has_children ) ? ' class="dropdown-togle" data-togle="dropdown"' : '';
+
+          $item_output = $args->before;
+          $item_output .= '<a' . $attributes . '>';
+          $item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
+          $item_output .= ( $depth == 0 && $args->walker->has_children ) ? '<b class="carret"></b></a>' : '</a>' ;
+          $item_output .= $args->after;
+
+          $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+
+        }//function start_el END
+        //START ELEMENT CUSTOM
+          
   
-    $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
-
-		$attributes = '';
-		foreach ( $atts as $attr => $value ) {
-			if ( ! empty( $value ) ) {
-				$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
-				$attributes .= ' ' . $attr . '="' . $value . '"';
-			}
-		}
-
-    $title = apply_filters( 'the_title', $item->title, $item->ID );
-    
-    $title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
-
-		$item_output = $args->before;
-		$item_output .= '<a'. $attributes .'>';
-		$item_output .= $args->link_before . $title . $args->link_after;
-		$item_output .= '</a>';
-    $item_output .= $args->after;
-    
-    $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-	}
-  // 
-  
-  }
+  } //class Walker_Naw_Side extends Walker_Nav_menu
   
   // <div class="panel panel-default">
   //   <div class="panel-heading" role="tab" id="headingOne">
