ee6ed19bf496e2d0804140f48d43c98bea24b1bc
Serhii Kirilenko <ksmua85@gmail.com>
2019-02-27 23:01:45 +0200

    Rename: sidebar->sidebar-left, infobar->sidebar-right

--- functions.php
+++ functions.php
@@ -28,22 +28,40 @@ function rda_styles() {
   }
 
   add_action('wp_enqueue_scripts', 'add_google_fonts');
+  
+  
+  /** Add Widgets support in themme */  
+  add_action('widget_init', 'register_my_widgets');
+
+
+  function register_my_widgets(){
+    register_sidebar(array(
+      'name'  => 'Right Sidebar',  
+      'id'    => 'right_Sidebar',
+      'description' => 'Опис сайдбару',
+      'before_widget' => '<div class="widget %2$s">',
+      'after_widget' => "</div>\n",
+      'before_title' => '<h5 class="widgettitle">',
+      'before_title' => "</h5>\n"
+    ));
+  }
+
+
+
 
   /** Add Menu support in themme */  
 
-
-
-  add_action('after_setup_theme', function(){
-    // add_theme_support('menus');
-    // register_nav_menu('header-menu', 'Меню в шапці сайту');
-    // register_nav_menu('footer-menu', 'Меню в підвалі сайту');
-    register_nav_menus([
-      'header-menu' => 'Меню в шапці сайту',
-      'footer-menu' => 'Меню в підвалі сайту',
-      'side-menu' => 'Меню в сайдбарі',
-      'right-side-menu' => 'Меню в правому сайдбарі'
-    ]);
-  });
+  // add_action('after_setup_theme', function(){
+  //   // add_theme_support('menus');
+  //   // register_nav_menu('header-menu', 'Меню в шапці сайту');
+  //   // register_nav_menu('footer-menu', 'Меню в підвалі сайту');
+  //   register_nav_menus([
+  //     'header-menu' => 'Меню в шапці сайту',
+  //     'footer-menu' => 'Меню в підвалі сайту',
+  //     'side-menu' => 'Меню в сайдбарі',
+  //     'right-side-menu' => 'Меню в правому сайдбарі'
+  //   ]);
+  // });
   
    
   /**
@@ -63,117 +81,119 @@ function rda_styles() {
     </ul> // end_lvl()
   </div>
   */
-  class Walker_Naw_Side extends Walker_Nav_menu {
+
+
+  // class Walker_Naw_Side extends Walker_Nav_menu {
    
-    function start_lvl(&$output, $depth = 0, $args = array() ){ // ul
-      $indent = str_repeat("\t", $depth);
-      $submenu = ($depth >= 0)? ' sub-menu' : ''; //inner ul classes
-      $collapse = ''; //($depth >= 0)? ' collapse' : '';
-      $output .= "\n$indent<ul class=\"myside-menu$submenu dropdown-container depth_$depth $collapse\" id=\"dropdown-container-id\" >\n"; // add id=\"dropdown-container-id\"
-    }//start_lvl
+  //   function start_lvl(&$output, $depth = 0, $args = array() ){ // ul
+  //     $indent = str_repeat("\t", $depth);
+  //     $submenu = ($depth >= 0)? ' sub-menu' : ''; //inner ul classes
+  //     $collapse = ''; //($depth >= 0)? ' collapse' : '';
+  //     $output .= "\n$indent<ul class=\"myside-menu$submenu dropdown-container depth_$depth $collapse\" id=\"dropdown-container-id\" >\n"; // add id=\"dropdown-container-id\"
+  //   }//start_lvl
   
-    /*
-    // START ELEMENT OROGINAL WALKER
+  //   /*
+  //   // START ELEMENT OROGINAL WALKER
   
-      public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-          if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
-            $t = '';
-            $n = '';
-          } else {
-            $t = "\t";
-            $n = "\n";
-          }
-          $indent = ( $depth ) ? str_repeat( $t, $depth ) : '';
+  //     public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+  //         if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+  //           $t = '';
+  //           $n = '';
+  //         } else {
+  //           $t = "\t";
+  //           $n = "\n";
+  //         }
+  //         $indent = ( $depth ) ? str_repeat( $t, $depth ) : '';
 
-          $classes = empty( $item->classes ) ? array() : (array) $item->classes;
-          // Here is @list-group-item@ clas added
-          $classes[] = 'list-group-item menu-item-' . $item->ID;
+  //         $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+  //         // Here is @list-group-item@ clas added
+  //         $classes[] = 'list-group-item menu-item-' . $item->ID;
 
-          $args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
+  //         $args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
 
-          $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
-          $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : ''; 
+  //         $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+  //         $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : ''; 
           
-          $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth );
-          $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+  //         $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth );
+  //         $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
 
-          $output .= $indent . '<li' . $id . $class_names .'>';
+  //         $output .= $indent . '<li' . $id . $class_names .'>';
 
-          $atts = array();
-          $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
-          $atts['target'] = ! empty( $item->target )     ? $item->target     : '';
-          $atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
-          $atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+  //         $atts = array();
+  //         $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+  //         $atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+  //         $atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+  //         $atts['href']   = ! empty( $item->url )        ? $item->url        : '';
         
-          $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+  //         $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
 
-          $attributes = '';
-          foreach ( $atts as $attr => $value ) {
-            if ( ! empty( $value ) ) {
-              $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
-              $attributes .= ' ' . $attr . '="' . $value . '"';
-            }
-          }
+  //         $attributes = '';
+  //         foreach ( $atts as $attr => $value ) {
+  //           if ( ! empty( $value ) ) {
+  //             $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+  //             $attributes .= ' ' . $attr . '="' . $value . '"';
+  //           }
+  //         }
 
-          $title = apply_filters( 'the_title', $item->title, $item->ID );
+  //         $title = apply_filters( 'the_title', $item->title, $item->ID );
           
-          $title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
+  //         $title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
 
-          $item_output = $args->before;
-          $item_output .= '<a'. $attributes .'>';
-          $item_output .= $args->link_before . $title . $args->link_after;
-          $item_output .= '</a>';
-          $item_output .= $args->after;
+  //         $item_output = $args->before;
+  //         $item_output .= '<a'. $attributes .'>';
+  //         $item_output .= $args->link_before . $title . $args->link_after;
+  //         $item_output .= '</a>';
+  //         $item_output .= $args->after;
           
-          $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-        }
-        */
-        //START ELEMENT OROGINAL WALKER
+  //         $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+  //       }
+  //       */
+  //       //START ELEMENT OROGINAL WALKER
         
-        //START ELEMENT CUSTOM
-        function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0) {
-          $indent = ($depth) ? str_repeat("\t", $depth) : '';
+  //       //START ELEMENT CUSTOM
+  //       function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0) {
+  //         $indent = ($depth) ? str_repeat("\t", $depth) : '';
 
-          $li_atributes = '';
-          $class_names = $value = '';
+  //         $li_atributes = '';
+  //         $class_names = $value = '';
 
-          $classes = empty( $item->classes ) ? array() : (array)$item->classes;
+  //         $classes = empty( $item->classes ) ? array() : (array)$item->classes;
 
-          $classes[] = ( $args->walker->has_children ) ? 'dropdown-btn' : '';
-          $classes[] = ( $item->curent || $item->curent_item_anchestor) ? 'active' : '';
-          $classes[] = 'menu-item-' . $item->ID; // Here @list-group-item@ clas added
-          if ( $depth && $args->walker->has_children ){
-            $classes[] = 'dropdown-submenu';
-          }
+  //         $classes[] = ( $args->walker->has_children ) ? 'dropdown-btn' : '';
+  //         $classes[] = ( $item->curent || $item->curent_item_anchestor) ? 'active' : '';
+  //         $classes[] = 'menu-item-' . $item->ID; // Here @list-group-item@ clas added
+  //         if ( $depth && $args->walker->has_children ){
+  //           $classes[] = 'dropdown-submenu';
+  //         }
 
-          $class_names = join(  ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
-          $class_names = 'class="' . esc_attr( $class_names ) . '"';
+  //         $class_names = join(  ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+  //         $class_names = 'class="' . esc_attr( $class_names ) . '"';
 
-          $id = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args);
-          $id = strlen( $id ) ? ' id="' . esc_attr($id) . '"' : '';
+  //         $id = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args);
+  //         $id = strlen( $id ) ? ' id="' . esc_attr($id) . '"' : '';
 
-          $output .= $indent . '<li' . $id . $value . $class_names . $li_atributes . '>';
+  //         $output .= $indent . '<li' . $id . $value . $class_names . $li_atributes . '>';
 
-          $attributes = ! empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
-          $attributes = ! empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
-          $attributes = ! empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
-          $attributes = ! empty($item->url) ? ' href="#"': ''; // . esc_attr($item->url) . '"' : '';// # no link addd now
+  //         $attributes = ! empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
+  //         $attributes = ! empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
+  //         $attributes = ! empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
+  //         $attributes = ! empty($item->url) ? ' href="#"': ''; // . esc_attr($item->url) . '"' : '';// # no link addd now
 
-          $attributes .= ( $args->walker->has_children ) ? ' class="dropdown-link" ' : ''; // < a href="..." class="dropdown-link"
+  //         $attributes .= ( $args->walker->has_children ) ? ' class="dropdown-link" ' : ''; // < a href="..." class="dropdown-link"
 
-          $item_output = $args->before;
-          $item_output .= '<a' . $attributes . '>';
-          $item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
-          $item_output .= ( $depth == 0 && $args->walker->has_children ) ? '<b class="carret"></b></a>' : '</a>' ;
-          $item_output .= $args->after;
+  //         $item_output = $args->before;
+  //         $item_output .= '<a' . $attributes . '>';
+  //         $item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
+  //         $item_output .= ( $depth == 0 && $args->walker->has_children ) ? '<b class="carret"></b></a>' : '</a>' ;
+  //         $item_output .= $args->after;
 
-          $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+  //         $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
 
-        }//function start_el END
-        //START ELEMENT CUSTOM
+  //       }//function start_el END
+  //       //START ELEMENT CUSTOM
           
   
-  } //class Walker_Naw_Side extends Walker_Nav_menu
+  // } //class Walker_Naw_Side extends Walker_Nav_menu
   
 
    
